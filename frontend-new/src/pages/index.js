5d37cf81247e3d79032bc5414ee6b5923bac24be
Florian Bär <florianbar01@gmail.com>
2024-01-06 22:05:48 +0200

    Added login

--- frontend-new/src/pages/index.js
+++ frontend-new/src/pages/index.js
@@ -8,17 +8,46 @@ export default function Home() {
   const [password, setPassword] = useState("");
 
   const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const [successMessage, setSuccessMessage] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(null);
 
   const authSubmitHandler = async (event) => {
     event.preventDefault();
 
+    setIsLoading(true);
+    setErrorMessage(null);
+    setSuccessMessage(null);
+
     if (isLoginMode) {
       // send login request
+      try {
+        const response = await fetch("http://localhost:5000/api/users/login", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            email,
+            password,
+          }),
+        });
+
+        const responseData = await response.json();
+        if (!response.ok) {
+          throw new Error(responseData.message);
+        }
+        console.log(responseData);
+        setIsLoading(false);
+        setSuccessMessage("You've successfully logged in!");
+        // auth.login();
+      } catch (err) {
+        console.log(err);
+        setIsLoading(false);
+        setErrorMessage(
+          err.message || "Something went wrong, please try again."
+        );
+      }
     } else {
       // send signup request
       try {
-        setIsLoading(true);
         const response = await fetch("http://localhost:5000/api/users/signup", {
           method: "POST",
           headers: { "Content-Type": "application/json" },
@@ -35,31 +64,56 @@ export default function Home() {
         }
         console.log(responseData);
         setIsLoading(false);
+        setSuccessMessage("You've successfully signed up!");
         // auth.login();
       } catch (err) {
         console.log(err);
         setIsLoading(false);
-        setError(err.message || "Something went wrong, please try again.");
+        setErrorMessage(
+          err.message || "Something went wrong, please try again."
+        );
       }
     }
   };
 
   return (
-    <>
+    <div className="p-4">
+      <button
+        onClick={() => {
+          setIsLoginMode(!isLoginMode);
+          setName("");
+          setEmail("");
+          setPassword("");
+        }}
+      >
+        {!isLoginMode ? "Login" : "Signup"}
+      </button>
+      <h1 className="text-3xl font-bold mb-4">
+        {isLoginMode ? "Login" : "Signup"}
+      </h1>
       {isLoading && <div>Loading</div>}
-      {error && (
-        <div className="text-red-800 font-bold bg-red-200 m-4 p-4">{error}</div>
+      {errorMessage && (
+        <div className="text-red-800 font-bold bg-red-200 mb-4 p-4">
+          {errorMessage}
+        </div>
       )}
-      <form method="POST" onSubmit={authSubmitHandler} className="p-4">
-        <input
-          type="text"
-          name="name"
-          placeholder="name"
-          className="block border border-slate-500 p-3 rounded mb-4 w-full"
-          value={name}
-          onChange={(event) => setName(event.target.value)}
-          disabled={isLoading}
-        />
+      {successMessage && (
+        <div className="text-green-800 font-bold bg-green-200 mb-4 p-4">
+          {successMessage}
+        </div>
+      )}
+      <form method="POST" onSubmit={authSubmitHandler}>
+        {!isLoginMode && (
+          <input
+            type="text"
+            name="name"
+            placeholder="name"
+            className="block border border-slate-500 p-3 rounded mb-4 w-full"
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+            disabled={isLoading}
+          />
+        )}
         <input
           type="email"
           name="email"
@@ -86,6 +140,6 @@ export default function Home() {
           Submit
         </button>
       </form>
-    </>
+    </div>
   );
 }
